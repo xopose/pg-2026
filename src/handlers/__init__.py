09cb4e4d46e4948c143d3880c9b9cd2f4aa596ea
ivanstrygin <feolius@gmail.com>
2026-05-17 18:22:32 +0300

    Fix dynamic handlers loading to load inner folders as wel

--- src/handlers/__init__.py
+++ src/handlers/__init__.py
@@ -1,9 +1,21 @@
 import importlib
-from pathlib import Path
+from pathlib import Path, PurePath
 
 package_dir = Path(__file__).parent
 
-for file in package_dir.glob("*.py"):
-    if file.name != "__init__.py":
-        module_name = file.stem
-        importlib.import_module(f".{module_name}", package=__name__)
+
+def load_all_in_package_folder(folder: PurePath, package: str) -> None:
+    if not (folder / "__init__.py").exists():  # type: ignore
+        return
+    for child in Path(folder).iterdir():
+        if child.name == "__init__.py":
+            continue
+        if child.name.endswith(".py"):
+            module_name = child.stem
+            importlib.import_module(f".{module_name}", package=package)
+            continue
+        if child.is_dir():
+            load_all_in_package_folder(child, package + "." + child.name)
+
+
+load_all_in_package_folder(package_dir, __name__)
